d6c560f39749869d0173a49aea8337e2f25d559b
Jesse West <jwest46@uncc.edu>
2016-02-28 13:20:58 -0500

    Started on the Summoner Match History form.

---
+++ League_of_Legends_Lookup/SummonerMatchHIstory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace League_of_Legends_Lookup
+{
+    public partial class SummonerMatchHIstory : Form
+    {
+        public SummonerMatchHIstory()
+        {
+            InitializeComponent();
+
+            //Change title
+            this.Text = "Summoner Match History";
+        }
+
+        //Overloaded constructor with summoner name
+        public SummonerMatchHIstory(string name)
+        {
+            InitializeComponent();
+
+            //Change title and capitlize first letter in the summoners name
+            this.Text = "Match History For " + name.First().ToString().ToUpper() + name.Substring(1);
+        }
+
+        private void SummonerMatchHIstory_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
